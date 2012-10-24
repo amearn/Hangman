df3ca4baed8b478aa77023c3aae6809a9efdf193
Pichaya Seankhum <bestza_bestza@hotmail.com>
2012-10-24 13:24:56 +0700

    อะไรซักอย่าง มึนละๆ

--- bababa/Hangman.java
+++ bababa/Hangman.java
@@ -1,3 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ *
+ * @author Administrator
+ */
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,10 +18,11 @@ import java.util.Random;
 /**
  * text based version of hangman
  */
-public class Hangman_1 {
+public class Hangman {
   public static void main( String[] args ) {
     HangmanSession hangmanSession = new HangmanSession();
     hangmanSession.play();
+    
   }
 }
 class HangmanSession {
@@ -19,6 +30,7 @@ class HangmanSession {
   private HiddenKeyword hiddenKeyword;
   private LetterBox letterBox;
   private int triesNumber = 7;
+  
 
   public HangmanSession() {
     player = new Player();
@@ -29,8 +41,8 @@ class HangmanSession {
 
   private void printState() {
     letterBox.print();
-    System.out.print( "Hidden word : " );
-    hiddenKeyword.print();
+    System.out.println( "Hidden word : " +s);
+    
     System.out.print( "Tries left: " + triesNumber + "\n<guess letter:>" );
   }
 
@@ -71,27 +83,15 @@ class HangmanSession {
   }
 }
 
-
-
 class HiddenKeyword {
-  private String fValue;
+  private String rword;
   private StringBuffer pValue;
   private int lfoundNumber = 0;
-  String rword;
-  private final int errors;
 
-  public HiddenKeyword() {
+  public HiddenKeyword(StringBuffer gword, String message, String information) {
       
       
-      
-      
-                  
-      		/* Setting the errors to 0 */
-                errors=0;
-				
-				
-		/* Enter the wordslist, separated by a | here: */
-                String str = "computer|radio|calculator|teacher|bureau|police|geometry|president|subject|country|enviroment|classroom|animals|province|month|politics|puzzle|instrument|kitchen|language|vampire|ghost|solution|service|software|virus25|security|phonenumber|expert|website|agreement|support|compatibility|advanced|search|triathlon|immediately|encyclopedia|endurance|distance|nature|history|organization|international|championship|government|popularity|thousand|feature|wetsuit|fitness|legendary|variation|equal|approximately|segment|priority|physics|branche|science|mathematics|lightning|dispersion|accelerator|detector|terminology|design|operation|foundation|application|prediction|reference|measurement|concept|perspective|overview|position|airplane|symmetry|dimension|toxic|algebra|illustration|classic|verification|citation|unusual|resource|analysis|license|comedy|screenplay|production|release|emphasis|director|trademark|vehicle|aircraft|experiment";
+      String str = "computer|radio|calculator|teacher|bureau|police|geometry|president|subject|country|enviroment|classroom|animals|province|month|politics|puzzle|instrument|kitchen|language|vampire|ghost|solution|service|software|virus25|security|phonenumber|expert|website|agreement|support|compatibility|advanced|search|triathlon|immediately|encyclopedia|endurance|distance|nature|history|organization|international|championship|government|popularity|thousand|feature|wetsuit|fitness|legendary|variation|equal|approximately|segment|priority|physics|branche|science|mathematics|lightning|dispersion|accelerator|detector|terminology|design|operation|foundation|application|prediction|reference|measurement|concept|perspective|overview|position|airplane|symmetry|dimension|toxic|algebra|illustration|classic|verification|citation|unusual|resource|analysis|license|comedy|screenplay|production|release|emphasis|director|trademark|vehicle|aircraft|experiment";
                 String[] temp;
 				
                 /* delimiter */
@@ -105,22 +105,34 @@ class HiddenKeyword {
 				
 		/* Generating random number */
 		int randomInt = randomGenerator.nextInt(temp.length);
-                rword = new String(temp[randomInt]);
-                char positions[] = new char[rword.length()];
+         rword = new String(temp[randomInt]);
+            char positions[] = new char[rword.length()];
+             System.out.print(rword);
                 for (int i=0; i<rword.length(); i++) {
-                        positions[i] = '_';
-    fValue = new String( "banana" );
-    pValue = new StringBuffer( "-------" );
+                        positions[i] = '-';
+                }
+                String s = new String(positions);
+                System.out.print(s);
+                gword = new StringBuffer(s);
+                tfLetter.setText("");
+  
+                // Delete the messages
+                message="";
+                information = "";
+                repaint();
+                
+    
+    
   }
 
-  public boolean found(){
+  public boolean found() {
     System.out.println( "Letters found:" + lfoundNumber + "/" + rword.length() );
     return ( lfoundNumber == rword.length() );
   }
 
 
   public boolean guess( char c ) {
-    int index = fValue.indexOf( c );
+    int index = rword.indexOf( c );
     if( index == -1 )
       return false;
     else {
@@ -130,15 +142,15 @@ class HiddenKeyword {
   }
 
   private int findOccurances( char c ) {
-    int idx = fValue.indexOf( c );
-    pValue.setCharAt( idx, fValue.charAt( idx ) );
+    int idx = rword.indexOf( c );
+    pValue.setCharAt( idx, rword.charAt( idx ) );
     int counter = 1;
     while( idx != -1 ) {
-      int idxx = fValue.indexOf( c, idx + 1 );
+      int idxx = rword.indexOf( c, idx + 1 );
       idx = idxx;
       if( idx != -1 ) {
         counter++;
-        pValue.setCharAt( idx, fValue.charAt( idx ) );
+        pValue.setCharAt( idx, rword.charAt( idx ) );
       }
     }
     return counter;
